fb8632d4aebfa03ac6f35f70b43e7fa648d52a3d
hsny91 <husniyesakmak.91@gmail.com>
2021-06-17 11:07:52 +0200

    variables.py was updated

--- basic/calculate.py
+++ basic/calculate.py
@@ -5,7 +5,26 @@ print(5 - 5)
 # Multiplication, division, modulo, and exponentiation
 print(3 * 5)
 print(10 / 2)
-print(18 % 7) # mod
-print(4 ** 2) # üs
+print(18 % 7) # Modulo:
+print(4 ** 2) # Exponentiation
 
 # How much is your $100 worth after 7 years?
+
+print(100 * 1.1 ** 7) 
+# Create a variable savings
+savings = 100
+
+# Create a variable growth_multiplier
+growth_multipler=1.1
+
+# Calculate result
+result=savings*growth_multipler**7
+
+# Print out result
+print(result)
+
+#BMI =WEIGHT/HEIGHT**2
+height=1.60
+weight=54.7
+BMI=weight/height**2
+print(BMI)
---
+++ basic/variables.py
@@ -0,0 +1,12 @@
+#int, or integer: a number without a fractional part. savings, with the value 100, is an example of an integer.
+#float, or floating point: a number that has both an integer and fractional part, separated by a point. 
+#str, or string: a type to represent text. You can use single or double quotes to build a string.
+#bool, or boolean: a type to represent logical values. Can only be True or False (the capitalization is important!).
+#you can find type of variables with type(name)
+
+
+savings = 100
+growth_multiplier = 1.1
+desc = "compound interest"
+print(type(savings))  #int
+print(type(growth_multiplier)) # float
