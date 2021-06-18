a66ba50a975198fa9afc0b897d3dcb8fb6af83d5
hsny91 <husniyesakmak.91@gmail.com>
2021-06-18 13:06:10 +0200

    list.py was updated

--- basic/list.py
+++ basic/list.py
@@ -74,3 +74,25 @@ x = [["a", "b", "c"],
 
 print(x[2][0])  #i expect g
 print(x[2][:2])  # I expect ['g','h']
+
+# Create the areas list
+areas = ["hallway", 11.25, "kitchen", 18.0, "living room", 20.0, "bedroom", 10.75, "bathroom", 9.50]
+
+# Correct the bathroom area
+
+areas[-1]=10.50
+# Change "living room" to "chill zone"
+areas[4]="chill zone"
+
+####### ADD ELEMENT AND CONCAT ######
+# Create the areas list and make some changes
+areas = ["hallway", 11.25, "kitchen", 18.0, "chill zone", 20.0,
+         "bedroom", 10.75, "bathroom", 10.50]
+
+# Add poolhouse data to areas, new list is areas_1
+areas_1=areas+["poolhouse", 24.5]
+
+# Add garage data to areas_1, new list is areas_2
+areas_2=areas_1+["garage",15.45]
+
+#####DELETE ELEMENT ##########
