59db99f0125774a4ca9c6be82143e0ed5461d712
hsny91 <husniyesakmak.91@gmail.com>
2021-06-19 10:30:23 +0200

    function.py was updated

--- basic/function.py
+++ basic/function.py
@@ -22,3 +22,36 @@ full_sorted2=sorted(full , reverse=True) ##buyukten kuucuge
 print(full_sorted)
 print(full_sorted2)
 
+####### STRING METHODS ############
+# str.upper()=> it is used for capitalize all letters
+# str.count('letter') => it is used to count the number of letter/letters inside str
+# string to experiment with: place
+place = "poolhouse"
+
+# Use upper() on place: place_up
+place_up=place.upper()
+
+# Print out place and place_up
+print(place_up)
+print(place)
+
+# Print out the number of o's in place
+print(place.count('o'))
+
+####### LIST METHODS ########
+# list.index(listElement) ==> it is used to find the index of element
+# list.count(listElement) ==> it is used to count the number of list element
+# list.append(list elemnet) ==> add lement into the list
+
+# Create list areas
+areas = [11.25, 18.0, 20.0, 10.75, 9.50]
+
+# Print out the index of the element 20.0
+print(areas.index(20.0))
+
+# Print out how often 9.50 appears in areas
+print(areas.count(9.50))
+
+# Use append twice to add poolhouse and garage size
+areas.append(24.5)
+areas.append(15.45)
