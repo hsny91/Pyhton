26e912d092f6b9a1ec4f3114605c90de42a68eae
hsny91 <husniyesakmak.91@gmail.com>
2021-06-20 16:38:54 +0200

    numpy.py was updated

--- basic/numpy.py
+++ basic/numpy.py
@@ -28,3 +28,43 @@ y[1]
 
 
 ##### 2D NUMPY ARRAY   ######
+
+# Create baseball, a list of lists
+baseball = [[180, 78.4],
+            [215, 102.7],
+            [210, 98.5],
+            [188, 75.2]]
+
+# Import numpy
+import numpy as np
+
+# Create a 2D numpy array from baseball: np_baseball
+np_baseball=np.array(baseball)
+
+# Print out the type of np_baseball
+print(type(np_baseball)) # numpy.ndarray
+print(np_baseball) #[[180.   78.4]
+                   # [215.  102.7]
+                   # [210.   98.5]
+                   # [188.   75.2]]
+
+# Print out the shape of np_baseball
+print(np_baseball.shape) #(4, 2)
+
+# Print out the 50th row of np_baseball
+print(np_baseball[49,:])
+
+# Select the entire second column of np_baseball: np_weight_lb
+np_weight_lb=np_baseball[:,1]
+
+# Print out height of 124th player
+print(np_baseball[123,0])
+
+# Print out addition of np_baseball and updated
+
+print(np_baseball+ updated)
+# Create numpy array: conversion
+conversion=np.array([0.0254,0.453592,1.])
+
+# Print out product of np_baseball and conversion
+print(np_baseball*conversion)
