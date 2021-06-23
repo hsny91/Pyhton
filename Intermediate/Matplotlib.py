d640e3a24d444ba36c6892cc7182720f9d7444fc
hsny91 <husniyesakmak.91@gmail.com>
2021-06-23 21:02:19 +0200

    histogram function was used

--- Intermediate/Matplotlib.py
+++ Intermediate/Matplotlib.py
@@ -6,7 +6,7 @@ pop=[10.5,20.5,8.5,9,10,10,10]
 plt.plot(year,pop)
 plt.show()  # show line
 
-import matplotlib.pyplot as plt
+
 
 gdp_cap=[2000,2001,2002,2003,2004,2005,2006]
 life_exp=[10.5,20.5,8.5,9,10,10,10]
@@ -17,3 +17,32 @@ plt.xscale('log')
 
 # Show plot
 plt.show()
+
+import matplotlib.pyplot as plt
+life_exp=[10.5,20.5,8.5,9,10,10,10]
+#########  HISTOGRAM  #########
+# plt.hist(value, bins=20)
+# plt.show() displays a plot;
+# bins ==> divide value how many piece
+#plt.clf() cleans it up again so you can start afresh.
+
+
+# Create histogram of life_exp data
+plt.hist(life_exp)
+
+# Display histogram
+plt.show()
+
+# Build histogram with 5 bins
+plt.hist(life_exp, bins=5)
+
+# Show and clean up plot
+plt.show()
+plt.clf()
+
+# Build histogram with 20 bins
+plt.hist(life_exp, bins=20)
+
+# Show and clean up again
+plt.show()
+plt.clf()
