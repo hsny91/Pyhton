82845c9e6fea4c62b7963564bbafcfefb35bb266
hsny91 <husniyesakmak.91@gmail.com>
2021-06-23 21:31:41 +0200

    Matpotlib.py was updated

--- Intermediate/Matplotlib.py
+++ Intermediate/Matplotlib.py
@@ -2,14 +2,59 @@ import matplotlib.pyplot as plt
 
 year=[2000,2001,2002,2003,2004,2005,2006]
 pop=[10.5,20.5,8.5,9,10,10,10]
+gdp_cap=[2000,2001,2002,2003,2004,2005,2006]
+life_exp=[10.5,20.5,8.5,9,10,10,10]
 
 plt.plot(year,pop)
 plt.show()  # show line
 
+####  Customized  ####
+## plt.xlabel('year')=> x axis labels 
+## plt.ylabel('year')=> y axis labels
+#  plt.title('title') => title
+# plt.yticks/yticks([0,1,2], ["one","two","three"]) ==>scale
 
+# Basic scatter plot, log scale
+plt.scatter(year, pop)
+plt.xscale('log') 
 
-gdp_cap=[2000,2001,2002,2003,2004,2005,2006]
-life_exp=[10.5,20.5,8.5,9,10,10,10]
+# Strings
+xlab = 'GDP per Capita [in USD]'
+ylab = 'Life Expectancy [in years]'
+title = 'World Development in 2007'
+
+# Add axis labels
+
+plt.xlabel(xlab)
+plt.ylabel(ylab)
+
+# Add title
+plt.title(title)
+
+# After customizing, display the plot
+plt.show()
+
+# Scatter plot
+plt.scatter(gdp_cap, life_exp)
+
+# Previous customizations
+plt.xscale('log') 
+plt.xlabel('GDP per Capita [in USD]')
+plt.ylabel('Life Expectancy [in years]')
+plt.title('World Development in 2007')
+
+# Definition of tick_val and tick_lab
+tick_val = [1000, 10000, 100000]
+tick_lab = ['1k', '10k', '100k']
+
+# Adapt the ticks on the x-axis
+plt.xticks(tick_val, tick_lab)
+
+# After customizing, display the plot
+plt.show()
+
+#### SCATTER ######
+
 plt.scatter(gdp_cap, life_exp)  # nokta nokta gosterim
 
 # Put the x-axis on a logarithmic scale
