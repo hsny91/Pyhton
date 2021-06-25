f8f7e4a5fc52be982f8dae4576dd61c59439123f
hsny91 <husniyesakmak.91@gmail.com>
2021-06-25 09:00:35 +0200

    variable.py was updated

--- basic/variables.py
+++ basic/variables.py
@@ -5,6 +5,31 @@
 #you can find type of variables with type(name)
 
 
+
+#Python allows you to assign values to multiple variables in one line:
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+x = y = z = "Orange" 
+
+print(x) # Orange
+print(y) # Orange
+print(z) # Orange
+
+fruits = ["apple", "banana", "banana"]
+x, y, z = fruits
+
+print(x) #apple
+print(y) #banana
+print(z) #banana
+
+#If you try to combine a string and a number, Python will give you an error:
+x = 5
+y = "John"
+#print(x + y) # error
+
+
 savings = 100
 growth_multiplier = 1.1
 result = 100 * 1.10 ** 7
@@ -25,6 +50,31 @@ doubledesc=desc+desc
 # Print out doubledesc(i expect compound interestcompound interest)
 print(doubledesc) 
 
+######  DATA TYPE #########
+
+#Text Type:	str
+#Numeric Types:	int, float, complex
+#Sequence Types:	list, tuple, range
+#Mapping Type:	dict
+#Set Types:	set, frozenset
+#Boolean Type:	bool
+#Binary Types:	bytes, bytearray, memoryview
+
+x = "Hello World"	#str	
+x = 20	#int	
+x = 20.5	#float	
+x = 1j	#complex	
+x = ["apple", "banana", "cherry"]	#list	
+x = ("apple", "banana", "cherry")	#tuple	
+x = range(6)	#range	
+x = {"name" : "John", "age" : 36}	#dict	
+x = {"apple", "banana", "cherry"}	#set	
+x = frozenset({"apple", "banana", "cherry"})	#frozenset	
+x = True	#bool	
+x = b"Hello"	#bytes	
+x = bytearray(5)	#bytearray	
+x = memoryview(bytes(5))	#memoryview
+
 
 ####   CONVERT TO TYPE   ######
 
@@ -37,3 +87,7 @@ pi_string = "3.1415926"
 
 # Convert pi_string into float: pi_float
 pi_float=float(pi_string)
+
+
+
+###### Python Casting ########
