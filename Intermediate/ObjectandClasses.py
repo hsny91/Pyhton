dea6882a9ee1d3bbc890cca8d117ab873e6fe271
hsny91 <husniyesakmak.91@gmail.com>
2021-10-15 12:45:44 +0200

    objectandclasses file was modified

--- Intermediate/ObjectandClasses.py
+++ Intermediate/ObjectandClasses.py
@@ -30,3 +30,11 @@ dir(RedCircle)
 
 # Call the method drawCircle
 RedCircle.drawCircle()
+
+# Use method to change the object attribute radius
+
+print('Radius of object:',RedCircle.radius)
+RedCircle.add_radius(2)
+print('Radius of object of after applying the method add_radius(2):',RedCircle.radius)
+RedCircle.add_radius(5)
+print('Radius of object of after applying the method add_radius(5):',RedCircle.radius)
