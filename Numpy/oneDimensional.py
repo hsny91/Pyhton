88759c0c518089b37a4d4f009f44fd07ec74cd37
hsny91 <husniyesakmak.91@gmail.com>
2021-10-17 15:53:11 +0200

    oneDimensional.py was updated

--- Numpy/oneDimensional.py
+++ Numpy/oneDimensional.py
@@ -20,4 +20,32 @@ print(nummpy_a) # [0 1 2 3 4]
 
 # Check the type of the array
 print(type(nummpy_a )) # numpy.ndarray
+
+# Check the type of the values stored in numpy array
+print(nummpy_a.dtype) # int64
  
+# Create a numpy array
+b = np.array([3.1, 11.02, 6.2, 213.2, 5.2])
+
+# Check the type of array
+print(type(b)) #numpy.ndarray
+
+# Check the value type
+print(b.dtype) #dtype('float64')
+
+##### INDEX SLICING #####
+
+# Create numpy array
+c = np.array([20, 1, 2, 3, 4])
+
+# Assign the first element to 100
+c[0] = 100
+print(c) # [100   1   2   3   4]
+
+# Slicing the numpy array
+d = c[1:4]
+print(d) # array([1, 2, 3])
+
+# Set the fourth element and fifth element to 300 and 400
+c[3:5] = 300, 400
+print(c) #[100   1   2 300 400]
