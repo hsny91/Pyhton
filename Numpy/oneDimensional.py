d1a1f3d2bc61781e80408cbc4544ad3187bf02e6
hsny91 <husniyesakmak.91@gmail.com>
2021-10-17 16:17:17 +0200

    oneDimensional.py was updated

--- Numpy/oneDimensional.py
+++ Numpy/oneDimensional.py
@@ -49,3 +49,70 @@ print(d) # array([1, 2, 3])
 # Set the fourth element and fifth element to 300 and 400
 c[3:5] = 300, 400
 print(c) #[100   1   2 300 400]
+
+####### BASIC OPERATION #########
+# Create a numpy array
+new_array= np.array([0, 1, 2, 3, 4])
+print(new_array)
+
+# Get the size of numpy array
+print(new_array.size) # 5
+
+# Get the number of dimensions of numpy array
+print(new_array.ndim) # 1
+
+# Get the shape/size of numpy array
+print(new_array.shape) # (5,)
+
+# Get the biggest value in the numpy array
+max_value=new_array.max()
+print(max_value) # 4
+
+# Get the smallest value in the numpy array
+min_value = new_array.min()
+print(min_value) # 0
+
+###### Numpy Array Operations ######
+
+u = np.array([1, 0])
+v = np.array([0, 1])
+
+# Numpy Array Addition
+z = u + v
+print(z) #  [1 1]
+
+# Numpy Array Multiplication
+multi_array = 2 * z
+print(multi_array) # [2 2]
+
+###### Product of Two Numpy Arrays ######
+
+# Create a numpy array
+first_array = np.array([1, 2])
+second_array= np.array([3, 2])
+
+# Calculate the production of two numpy arrays
+third_array = first_array*second_array
+print(third_array) #  [3 4]
+
+# Calculate the dot product
+print(np.dot(first_array, second_array)) # 7
+
+# Add the constant to array
+print(third_array+1) # [4 5]
+
+# Get the mean of numpy array(avarage)
+mean = third_array.mean()
+print(mean) #  3.5
+
+##### Mathematical Functions #### 
+
+# The value of pi
+print(np.pi) #  3.14159265359
+
+# Create the numpy array in radians
+k = np.array([0, np.pi/2 , np.pi])
+
+# Calculate the sin of each elements
+l = np.sin(k)
+print(l) # [  0.00000000e+00   1.00000000e+00   1.22464680e-16]
